0d01aa8395feddf37b30d5761f40a65030cf8661
HanMortis <3361947840@qq.com>
2026-06-19 17:30:02 +0800

    Update prisma.config.ts to use DATABASE_URL env var

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,12 +2,14 @@
 // npm install --save-dev prisma dotenv
 import { defineConfig } from "prisma/config";
 
+const databaseUrl = process.env["DATABASE_URL"] || "file:./dev.db";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: "file:./dev.db",
+    url: databaseUrl,
   },
 });
